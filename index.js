bda4250ccaa2f2a095f1e539b75774282d1f656a
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-04-24 18:28:44 -0700

    added 404 page route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Homepage')
 }) 
 
+app.get('*', (req, res) => {
+    res.status(404).send('404')
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`I'M ALIVE ON ${process.env.PORT}`)
 })
