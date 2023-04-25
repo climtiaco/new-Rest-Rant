bf8ccc6b5b88e9cf578c9f2fb26eb25cd14cdeae
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-04-24 21:12:40 -0700

    Adding home page view

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 
+//Middleware
+// app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 //Router
 app.use('/places', require('./controllers/places'))
 
+//Routes
 app.get('/', (req, res) => {
-    res.send('Homepage')
+    // Res.render ALWAYS looks for the file name of jsx file in your views folder.
+    res.render('home')
 }) 
 
 app.get('*', (req, res) => {
     res.status(404).send('404')
 })
 
+//Listen
 app.listen(process.env.PORT, () => {
     console.log(`I'M ALIVE ON ${process.env.PORT}`)
 })
