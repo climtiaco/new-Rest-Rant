1c6af18356ffdbf79a9e854959287e8d011d6345
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-04-25 03:35:01 -0700

    Added places view w Mock data

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,22 @@
 const router = require('express').Router()
 
+// GET /places
 router.get('/', (req, res) => {
-    res.send('GET /places')
-})
+    let places = [{
+        name: 'Hearthstone',
+        city: 'Las Vegas',
+        state: 'Nevada',
+        cuisines: 'American',
+        pic: 'https://www.redrockresort.com/wp-content/uploads/2020/04/RR-Hearthstone-01-scaled.jpg'
+    }, {
+        name: 'North Italia',
+        city: 'Las Vegas',
+        state: 'Nevada',
+        cuisines: 'Italian',
+        pic: 'https://cdn.vox-cdn.com/thumbor/dox3bJKz25UiYgHuDexAdZPD-Wo=/0x0:2000x1333/1200x800/filters:focal(840x507:1160x827)/cdn.vox-cdn.com/uploads/chorus_image/image/57976773/North_Italia___Dining_Room_4.0.jpg'
+
+    }]
+    res.render('places/index', { places })
+});
 
 module.exports = router
