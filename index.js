c093238a7325cc8de09af645981358ee14107d8c
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-04-28 16:38:57 -0700

    Adding styles to Rest-Rant

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 
-//Middleware
-// app.set('views', __dirname + '/views');
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
 
 //Router
 app.use('/places', require('./controllers/places'))
+
 
 //Routes
 app.get('/', (req, res) => {
     // Res.render ALWAYS looks for the file name of jsx file in your views folder.
     res.render('home')
 }) 
+
+//Middleware
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+//Added this line below when we created the mkdir 'public'
+app.use(express.static('public'))
 
 app.get('*', (req, res) => {
     res.render('error404')
---
+++ views/places/index.jsx
@@ -0,0 +1,24 @@
+const React = require('react')
+// the periods in this '../default'. One period steps up the tree once, two periods steps up the tree twice, and so since it's in 'places' folder, it needed to step up twice before being able to be read.
+const Def = require('../default')
+
+function index ({ places }) {
+    let placesFormatted = places.map(place => {
+        return (
+            <div>
+                <h2>{place.name}</h2>
+                <img src={place.pic} alt={place.name} />
+            </div>
+        )
+    })
+    return (
+        <Def>
+            <main>
+                <h1>index</h1>
+                {placesFormatted}
+            </main>
+        </Def>
+    )
+}
+
+module.exports = index
